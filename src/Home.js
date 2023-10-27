e33080adcc82846db073f62ae9650c33fa166e0b
parvathy <nandakumarparvathy97@gmail.com>
2023-10-27 13:25:32 -0400

    issue fix

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,8 @@ function Home() {
       <Fragment id="home">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-sm-4 order-md-2 cont mypic-home-div"><img class="mypic-home" src={require('./portfolio-pics/mypic-home.jpg')} /></div>
-                <div class="col-sm-8 order-md-1 cont content-home-div">
+                <div class="col-sm-4 order-sm-2 cont mypic-home-div"><img class="mypic-home" src={require('./portfolio-pics/mypic-home.jpg')} /></div>
+                <div class="col-sm-8 order-sm-1 cont content-home-div">
                     <p class="hello-world"><span>Hello </span><span>World!</span></p> 
                     <p class="name-intro"><span>This is </span><span>PARVATHY NANDAKUMAR</span></p>
                 </div>
